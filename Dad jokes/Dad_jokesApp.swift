bec0beffb85f9a599b2cc2edbe3bca904a2f254a
clf94-dev <clf94@hotmail.es>
2023-07-15 15:26:09 +0200

    Initial Commit

---
+++ Dad jokes/Dad_jokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dad_jokesApp.swift
+//  Dad jokes
+//
+//  Created by Carmen Lucas on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Dad_jokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
