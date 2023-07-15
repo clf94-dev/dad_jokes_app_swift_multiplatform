431e11accad3c38d0d7b934608df807eef0bcf8b
clf94-dev <clf94@hotmail.es>
2023-07-15 17:46:28 +0200

    - add iOS target - add iOS assets

---
+++ Dad Jokes iOS/iOSEntry.swift
@@ -0,0 +1,17 @@
+//
+//  Dad_Jokes_iOSApp.swift
+//  Dad Jokes iOS
+//
+//  Created by Carmen Lucas on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Dad_Jokes_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
